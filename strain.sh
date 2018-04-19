92619aae440ee1a8e9fe90893b07215ccaeb66bc
Tomas Bruna <bruna.tomas@gatech.edu>
2018-04-19 11:58:47 -0400

    Make strain seeker script portable

--- strain.sh
+++ strain.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 if [ "$#" -eq 0 ]
-	then echo "  ** INVALID USAGE**. Check -h for help " 
-	exit 
+	then echo "  ** INVALID USAGE**. Check -h for help "
+	exit
 fi
 
 while getopts "i:h" arg; do
@@ -15,15 +15,18 @@ done
 shift $((OPTIND - 1))
 
 if [ "$help" == "TRUE" ]
-then echo "  Usage options: ./strain.sh -i <filename> 
+then echo "  Usage options: ./strain.sh -i <filename>
    -i   input file name
-   -h   help 
+   -h   help
     example:
 	./strain.sh -i SRR12345.fasta
-	./strain.sh -i SRR12345.fq"	 
-exit 
+	./strain.sh -i SRR12345.fq"
+exit
 fi
 
+src="${BASH_SOURCE[0]}"
+src_dir=`dirname $src`
+cd $src_dir/phylogeny/strain_detect
 
 perl seeker.pl $input -d ./database/ -verbose -o $input"_strain_result"
 
